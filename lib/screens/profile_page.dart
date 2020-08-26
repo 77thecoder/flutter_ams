8fb6ebb9f2298089d64a1a8a2182f50d92fec7b4
devspo <devspo@inbox.ru>
2020-08-26 06:39:39 +0400

    Поправил выход
    Вынес функционал в отдельный сервис
    Выход из drawer работает

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:nvbs_ams/screens/login_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:nvbs_ams/widgets/drawer.dart';
+import 'package:nvbs_ams/services/exit.dart';
 
 class ProfilePage extends StatelessWidget {
+  ProfilePage({Key key}) : super(key: key);
+
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      drawer: DrawerApp(),
       appBar: AppBar(
         automaticallyImplyLeading: false,
+        backgroundColor: Colors.red,
         centerTitle: true,
         title: Text(
           'Профиль',
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          // onPressed: () => Scaffold.of(context).openEndDrawer(),
+          onPressed: () => _scaffoldKey.currentState.openDrawer(),
         ),
         actions: <Widget>[
           IconButton(
@@ -18,16 +31,9 @@ class ProfilePage extends StatelessWidget {
               Icons.exit_to_app,
               color: Colors.white,
             ),
-            onPressed: () async {
-              SharedPreferences prefs = await SharedPreferences.getInstance();
-              prefs.remove('user.isAuth');
-              prefs.remove('user.login');
-              prefs.remove('user.tn');
-              prefs.remove('user.fio');
-              prefs.remove('user.email');
-              prefs.remove('user.position');
-              prefs.remove('user.department');
-              Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+            onPressed: () {
+              Future<bool> exit = AppService.exit();
+              exit.then((value) => Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage())));
             },
           )
         ],
