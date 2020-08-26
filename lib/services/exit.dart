4e3b64b3a9fc853793d694851f419e219094b799
devspo <devspo@inbox.ru>
2020-08-26 06:40:30 +0400

    Поправил выход + Drawer
    Вынес функционал в отдельный сервис
    Выход из drawer работает

---
+++ lib/services/exit.dart
@@ -0,0 +1,16 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+
+class AppService {
+  static Future<bool> exit() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('user.isAuth');
+    prefs.remove('user.login');
+    prefs.remove('user.tn');
+    prefs.remove('user.fio');
+    prefs.remove('user.email');
+    prefs.remove('user.position');
+    prefs.remove('user.department');
+    return true;
+  }
+}
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:nvbs_ams/screens/login_page.dart';
+import 'package:nvbs_ams/services/exit.dart';
+
+class DrawerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('Подчиненные'),
+            onTap: () {
+              //
+            },
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Выйти'),
+            onTap: () {
+              Future<bool> exit = AppService.exit();
+              exit.then((value) => Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage())));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
